f9b36bfe07899f8912f78a515fa1df815acc6ef3
slvamegestic <35224544+slvamegestic@users.noreply.github.com>
2020-12-30 09:59:19 +0530

    new commit for linechart by selva

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { Routes, RouterModule } from '@angular/router';
 import { ChartsModule } from 'ng2-charts';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LineChartComponent } from './line-chart/line-chart.component';
 import { DoughnutChartComponent } from './doughnut-chart/doughnut-chart.component';
 
+const appRoutes: Routes = [
+  {
+    path :'line-chart', component:LineChartComponent
+  }];
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,7 +20,7 @@ import { DoughnutChartComponent } from './doughnut-chart/doughnut-chart.componen
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ChartsModule
+    ChartsModule,RouterModule.forRoot(appRoutes,{ useHash: true })
   ],
   providers: [],
   bootstrap: [AppComponent]
